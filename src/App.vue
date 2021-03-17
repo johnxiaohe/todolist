d050c528b90a62fa68922607871d148e541273f6
he_xy6 <he_xy6@hdec.com>
2021-03-17 12:52:24 +0800

    :init: 完成初步todolist功能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-bind:msg="msg"/>
+  </div> -->
+  <div id="app">
+      <span id="title">todos</span>
+      <div id="allbox">
+        <div id="top">
+          <button id="listbtn" @click="btnmethod" ><i :class="[btnSylom]" style="font-size:30px; font-weight:bold; color:#4040448e"></i></button>
+          <input placeholder="What needs to be done?" v-model="currentInput" @keyup.enter="createTodo">
+        </div>
+        <!-- 循环添加子组件 -->
+        <div id="todos">
+          <Todo v-for="item in showlist" :key="item.id" :todo="item" class="todos-for" @remove="remove" @checkmethod="checkmethod"/>
+        </div>
+        <div id="follor">
+          <span class="follor-num"> {{ todoSum }} items left</span>
+            <span class="follor-btn" @click="selectStatus('all')">All</span>
+            <span class="follor-btn" @click="selectStatus('active')">Active</span>
+            <span class="follor-btn" @click="selectStatus('completed')">Completed</span> 
+
+            <span class="follor-btn" style="float:right" @click="clear">Clear completed</span>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import './assets/app.css'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Todo
+  },
+  data() {
+    return {
+      currentInput: "",
+      index: 0,
+      todos: [],
+      btnSylom: '',
+      currentSelect: 'all'
+    }
+  },
+  computed: {
+    todoSum : function(){
+      return this.todos.length
+    },
+    showlist : function(){
+      if(this.currentSelect === 'active'){
+        return this.todos.filter(i => { return !i.complete; })
+      }else if(this.currentSelect === 'completed'){
+        return this.todos.filter(i => { return i.complete; })
+      }
+      return this.todos;
+    }
+  },
+  beforeMount: function() {
+    this.btnSylom = 'el-icon-arrow-right'
+  },
+  methods: {
+      createTodo: function(){
+        if(!this.currentInput){
+          return
+        }
+        var todo = {
+          name: this.currentInput
+          ,complete: false
+          ,id: this.index
+        }
+        this.todos.push(todo)
+        this.index +=1
+        this.currentInput = ""
+      },
+      btnmethod: function(){
+        if(this.btnSylom === 'el-icon-arrow-right'){
+          this.btnSylom = "el-icon-arrow-down"
+        }else{
+          this.btnSylom = "el-icon-arrow-right"
+        }
+      },
+      clear: function(){
+        this.todos = this.todos.filter(item =>{
+          return !item.complete;
+        })
+      },
+      remove: function(todo){
+        this.todos = this.todos.filter(item =>{
+          return item.id != todo.id;
+        })
+      },
+      checkmethod: function(todo){
+        todo.complete = !todo.complete;
+      },
+      selectStatus: function(status){
+          this.currentSelect = status
+      }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="todo">
+        <input class="toggle" type="checkbox" @click="checkMethod" :id="todo.id"/>
+        <label class="text" :for="todo.id" :style="todo.complete?'text-decoration: line-through; color: slategray;':''">{{ todo.name }}</label>
+        <button class="distorybtn" @click="$emit('remove', todo)"><i class="el-icon-delete" /></button>
+    </div>
+</template>
+
+<script>
+import '../assets/todo.css'
+
+export default {
+    name: 'Todo',
+    props: {
+        todo: Object
+    },
+    methods: {
+        checkMethod: function(){
+            this.$emit('checkmethod', this.todo)
+        }
+    }
+}
+</script>
+
+<style>
+.todo {
+    width: 100%;
+    height: 80px;
+    border-top: darkgreen;
+    border-top-style: solid;
+    /* border-style: solid; */
+}
+
+.toggle {
+    width: 15%;
+    margin-top: 0px;
+    display: inline-block;
+    vertical-align: top;
+}
+
+.text {
+    width: 74%;
+    box-sizing: border-box;
+    display: inline-block;
+    line-height: 80px;
+    vertical-align: top;
+    
+}
+
+.distorybtn {
+    width: 10%;
+    height: 100%;
+    font-size: 30px;
+    display: inline-block;
+    vertical-align: top;
+    outline: none;
+    border: none;
+    background: white;
+    cursor: pointer;
+}
+
+
+</style>
